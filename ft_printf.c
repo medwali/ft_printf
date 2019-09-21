26b8f1aa71726a63e1a1d3e3ae4ac7fe31ef6905
youssef lagtab <yousseflagtab@gmail.com>
2019-09-21 23:31:08 +0100

    fixed variable name: pareser_ret -> parse_ret

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/20 21:20:03 by mel-idri          #+#    #+#             */
-/*   Updated: 2019/09/21 22:21:37 by mel-idri         ###   ########.fr       */
+/*   Updated: 2019/09/21 23:29:53 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,24 @@
 int ft_printf(char *format, ...)
 {
 	va_list ap;
-	int		pareser_ret;
-	int		ret;
+	int parse_ret;
+	int ret;
 
 	ret = 0;
-	pareser_ret = 0;
+	parse_ret = 0;
 	if (!format)
 		return (-1);
 	va_start(ap, format);
 	while (*format)
 	{
 		if (*format == '%')
-			pareser_ret = parse_conversion(&format, &ap);
-		if (pareser_ret == -1)
-			break ;
+			parse_ret = parse_conversion(&format, &ap);
+		if (parse_ret == -1)
+			break;
 		else
-			ret += pareser_ret;
+			ret += parse_ret;
 		ret += write(1, format++, 1);
 	}
 	va_end(ap);
-	return (pareser_ret == -1 ? -1 : ret);
+	return (parse_ret == -1 ? -1 : ret);
 }
