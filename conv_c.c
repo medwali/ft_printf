3f9ffa449671b727d1851700f619b8f29a4a52ce
Mohamed El idrissi el wali <mel-idri@e2r4p9.1337.ma>
2019-09-23 04:55:55 +0100

    fix conv_c

--- conv_c.c
+++ conv_c.c
@@ -6,7 +6,7 @@
 /*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 21:48:30 by mel-idri          #+#    #+#             */
-/*   Updated: 2019/09/23 00:11:28 by mel-idri         ###   ########.fr       */
+/*   Updated: 2019/09/23 04:55:16 by mel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	conv_c(t_conv_spec *conv_spec, va_list *ap)
 {
 	char c;
 
-	c = (char)va_arg(ap, char);
+	c = (char)va_arg(*ap, char);
 	if (conv_spec->width && !(conv_spec->flags & FLAG_MINUS))
 		ft_putnchar(' ', conv_spec->width - 1);
 	write(1, &c, 1);
