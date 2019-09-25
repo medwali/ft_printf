8da701d65444943bff77861cd7fa88168adf029b
youssef lagtab <yousseflagtab@gmail.com>
2019-09-25 15:29:09 +0100

    moved put_octal to util.c, added its prototype to header

--- util.c
+++ util.c
@@ -3,17 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   util.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 16:31:50 by mel-idri          #+#    #+#             */
-/*   Updated: 2019/09/21 16:34:05 by mel-idri         ###   ########.fr       */
+/*   Updated: 2019/09/25 15:28:00 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int is_in_str(int c, char *str)
 {
-	while(*str)
+	while (*str)
 		if (*str++ == c)
 			return (1);
 	return (0);
+}
+
+void put_octal(unsigned long long decimal)
+{
+	if (decimal / 8)
+		put_octal(decimal / 8);
+	ft_putchar(decimal % 8 + '0');
 }
