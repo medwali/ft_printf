0dc5ed1e176449b8fa4f7c9428048549546661d1
Youssef Lagtab <yousseflagtab@gmail.com>
2019-11-20 12:13:42 +0100

    added bigint_is_zero function

--- bigint/bigint.h
+++ bigint/bigint.h
@@ -6,13 +6,13 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 08:23:38 by mel-idri          #+#    #+#             */
-/*   Updated: 2019/11/19 10:04:17 by ylagtab          ###   ########.fr       */
+/*   Updated: 2019/11/20 10:54:44 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BIGINT_H
-#define BIGINT_H
-#define IS_NULL(x) (x == NULL)
+# define BIGINT_H
+# define IS_NULL(x) (x == NULL)
 
 typedef struct	s_bigint
 {
@@ -26,5 +26,6 @@ t_bigint	*bigint_mult(t_bigint *a, t_bigint *b);
 t_bigint	*bigint_from_uint128(__uint128_t unbr);
 t_bigint	*bigint_power(unsigned int b, unsigned int exp);
 void		bigint_print(t_bigint *bg_int);
+int			bigint_is_zero(t_bigint *bg_int);
 
 #endif
--- bigint/bigint_util.c
+++ bigint/bigint_util.c
@@ -6,11 +6,22 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 09:49:56 by ylagtab           #+#    #+#             */
-/*   Updated: 2019/11/19 10:00:12 by ylagtab          ###   ########.fr       */
+/*   Updated: 2019/11/20 10:55:11 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
+
+int		bigint_is_zero(t_bigint *bg_int)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < bg_int->length)
+		if (bg_int->digits[i] != 0)
+			return (0);
+	return (1);
+}
 
 void	bigint_print(t_bigint *bg_int)
 {
