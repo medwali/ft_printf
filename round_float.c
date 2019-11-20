1fd6834b042d1e3576e49e30c9eac322bad1be63
Youssef Lagtab <yousseflagtab@gmail.com>
2019-11-20 12:32:47 +0100

    fixed round_float
    -condition for index to avoid segfault
    -fixed some indexs and lengths

--- round_float.c
+++ round_float.c
@@ -6,7 +6,7 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 21:18:31 by mel-idri          #+#    #+#             */
-/*   Updated: 2019/11/19 20:53:47 by ylagtab          ###   ########.fr       */
+/*   Updated: 2019/11/20 11:43:05 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,50 @@
 
 void	increment_float_part(t_bigint **whole, t_bigint **frac, int precision)
 {
-	int			frac_part_length;
-	t_bigint	*added_number;
+	unsigned int	frac_part_length;
+	t_bigint		*added_number;
 
 	if (precision == 0)
-		*whole = bigint_add(*whole, bigint_from_long(1));
-	else if (precision >= 0)
 	{
-		frac_part_length = (*frac)->length;
-		added_number = bigint_new(precision);
-		added_number->digits[precision - 1] = 1;
-		*frac = bigint_add(*frac, added_number);
-		if (frac_part_length < (*frac)->length)
-		{
-			*whole = bigint_add(*whole, bigint_from_long(1));
-			*frac = bigint_new(1);
-		}
+		*whole = bigint_add(*whole, bigint_from_uint128(1));
+		(*frac)->length = 0;
+		return ;
+	}
+	frac_part_length = (*frac)->length;
+	added_number = bigint_new((*frac)->length - precision + 1);
+	added_number->digits[added_number->length - 1] = 1;
+	*frac = bigint_add(*frac, added_number);
+	if (frac_part_length < (*frac)->length)
+	{
+		*whole = bigint_add(*whole, bigint_from_uint128(1));
+		*frac = bigint_new(1);
 	}
 }
 
 void	round_float(t_bigint **whole, t_bigint **frac, int precision)
 {
-	int	is_one_added;
 	int	i;
+	int	index;
 
-	if ((*frac)->length <= precision)
+	index = (*frac)->length - precision - 1;
+	if (index < 0)
 		return ;
-	if ((*frac)->digits[precision] > 5)
-		is_one_added = precision == 0 ? 2 : 1;
-	increment_float_part(whole, frac, precision);
-	if ((*frac)->digits[precision] == 5)
-		if (precision == 0 && (*whole)->digits[0] % 2)
-			increment_float_part(whole, frac, precision);
-		else if (precision != 0 && (*frac)->digits[precision - 1] % 2)
+	if ((*frac)->digits[index] > 5)
+		increment_float_part(whole, frac, precision);
+	if ((*frac)->digits[index] == 5)
+	{
+		if ((precision == 0 && (*whole)->digits[0] % 2) ||
+			(precision != 0 && (*frac)->digits[index + 1] % 2))
 			increment_float_part(whole, frac, precision);
 		else
 		{
-			i = precision;
-			while (++i < (*frac)->length)
+			i = index;
+			while (--i >= 0)
 				if ((*frac)->digits[i])
 				{
 					increment_float_part(whole, frac, precision);
 					break ;
 				}
 		}
+	}
 }
