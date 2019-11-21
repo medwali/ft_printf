3c1cde7395b3d41ff67d8a44507078ed794f80a0
Youssef Lagtab <yousseflagtab@gmail.com>
2019-11-21 19:48:01 +0100

    norminette

--- bigint/bigint.h
+++ bigint/bigint.h
@@ -6,7 +6,7 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 08:23:38 by mel-idri          #+#    #+#             */
-/*   Updated: 2019/11/20 10:54:44 by ylagtab          ###   ########.fr       */
+/*   Updated: 2019/11/21 19:36:35 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ typedef struct	s_bigint
 	unsigned	length;
 }				t_bigint;
 
-t_bigint	*bigint_new(unsigned int size);
-t_bigint	*bigint_add(t_bigint *a, t_bigint *b);
-t_bigint	*bigint_mult(t_bigint *a, t_bigint *b);
-t_bigint	*bigint_from_uint128(__uint128_t unbr);
-t_bigint	*bigint_power(unsigned int b, unsigned int exp);
-void		bigint_print(t_bigint *bg_int);
-int			bigint_is_zero(t_bigint *bg_int);
+t_bigint		*bigint_new(unsigned int size);
+t_bigint		*bigint_add(t_bigint *a, t_bigint *b);
+t_bigint		*bigint_mult(t_bigint *a, t_bigint *b);
+t_bigint		*bigint_from_uint128(__uint128_t unbr);
+t_bigint		*bigint_power(unsigned int b, unsigned int exp);
+void			bigint_print(t_bigint *bg_int);
+int				bigint_is_zero(t_bigint *bg_int);
 
 #endif
