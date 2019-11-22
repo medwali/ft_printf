a6e23f6f66c9fef5f2fe5c4a17145d9cee85bfd3
Mohamed El idrissi el wali <mel-idri@e1r7p13.1337.ma>
2019-11-22 16:49:06 +0100

    fix typo in round_float

--- round_float.c
+++ round_float.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   round_float.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 21:18:31 by mel-idri          #+#    #+#             */
-/*   Updated: 2019/11/22 15:05:50 by ylagtab          ###   ########.fr       */
+/*   Updated: 2019/11/22 16:48:30 by mel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int		increment_float(t_bigint **whole, t_bigint **frac, int precision)
 {
 	unsigned int	frac_part_length;
 	t_bigint		*added_number;
-./
 	if (precision == 0)
 	{
 		if ((*whole = bigint_add(*whole, bigint_from_uint128(1))) == NULL)
