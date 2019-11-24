b1bb4c2116eb0538757b4a969ea343ca719c17fc
Youssef Lagtab <yousseflagtab@gmail.com>
2019-11-24 19:01:02 +0100

    finished conv_u with tester :D

--- conv_u.c
+++ conv_u.c
@@ -6,38 +6,84 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 15:44:55 by ylagtab           #+#    #+#             */
-/*   Updated: 2019/11/21 14:29:28 by ylagtab          ###   ########.fr       */
+/*   Updated: 2019/11/24 16:48:33 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
-static int	get_printed_len(int nbr_len, int spaces, int zeros)
+static int	get_spaces(t_conv_spec *conv_spec, t_specs specs)
 {
-	spaces = spaces > 0 ? spaces : 0;
-	zeros = zeros > 0 ? zeros : 0;
-	return (spaces + zeros + nbr_len);
+	int width;
+	int precision;
+	int ret;
+
+	precision = conv_spec->precision;
+	width = conv_spec->width;
+	if (conv_spec->is_pset && conv_spec->precision == 0 && specs.unbr == 0)
+		specs.unbr_len = 0;
+	if (conv_spec->is_pset == 0 && (conv_spec->flags & FLAG_ZERO))
+		return (0);
+	ret = width - ft_max(precision, specs.unbr_len) -
+		(specs.sign || (conv_spec->flags & (FLAG_PLUS | FLAG_SPACE)));
+	return (ret > 0 ? ret : 0);
+}
+
+static int	get_zeros(t_conv_spec *conv_spec, t_specs specs)
+{
+	int width;
+	int precision;
+	int ret;
+
+	precision = conv_spec->precision;
+	width = conv_spec->width;
+	if (conv_spec->is_pset)
+	{
+		ret = precision - specs.unbr_len;
+		return (ret > 0 ? ret : 0);
+	}
+	if (conv_spec->flags & FLAG_ZERO)
+	{
+		ret = width - specs.unbr_len -
+			(specs.sign || (conv_spec->flags & (FLAG_PLUS | FLAG_SPACE)));
+		return (ret > 0 ? ret : 0);
+	}
+	return (0);
+}
+
+static int	get_printed_len(t_conv_spec *conv_spec, t_specs specs)
+{
+	if (conv_spec->is_pset && conv_spec->precision == 0 && specs.unbr == 0)
+		specs.unbr_len = 0;
+	return (specs.spaces + specs.zeros + specs.unbr_len +
+			(specs.sign || (conv_spec->flags & (FLAG_PLUS | FLAG_SPACE))));
+}
+
+static void	read_unbr(va_list *ap, t_length len, t_specs *specs)
+{
+	specs->unbr = read_uint(ap, len);
+	specs->unbr_len = specs->unbr == 0 ? 1 : ft_nbrlen(specs->unbr);
+	specs->sign = 0;
 }
 
 int			conv_u(t_conv_spec *conv_spec, va_list *ap)
 {
-	unsigned long long	unbr;
-	int					nbr_len;
-	int					spaces;
-	int					zeros;
+	t_specs specs;
 
-	unbr = read_uint(ap, conv_spec->length);
-	nbr_len = ft_nbrlen(unbr);
-	if (unbr == 0 && conv_spec->is_pset == 1 && conv_spec->precision == 0)
-		nbr_len = 0;
-	spaces = get_spaces_len(conv_spec, 1, nbr_len);
-	zeros = get_zeros_len(conv_spec, 1, nbr_len);
+	read_unbr(ap, conv_spec->length, &specs);
+	specs.spaces = get_spaces(conv_spec, specs);
+	specs.zeros = get_zeros(conv_spec, specs);
 	if ((conv_spec->flags & FLAG_MINUS) == 0)
-		ft_putnchar(' ', spaces);
-	ft_putnchar('0', zeros);
-	if (!(unbr == 0 && conv_spec->is_pset == 1 && conv_spec->precision == 0))
-		ft_putunbr(unbr);
+		ft_putnchar(' ', specs.spaces);
+	if ((conv_spec->flags & FLAG_SPACE))
+		ft_putchar(' ');
+	if ((conv_spec->flags & FLAG_PLUS))
+		ft_putchar('+');
+	ft_putnchar('0', specs.zeros);
+	if (!(conv_spec->is_pset && conv_spec->precision == 0 && specs.unbr == 0))
+		ft_putunbr(specs.unbr);
 	if (conv_spec->flags & FLAG_MINUS)
-		ft_putnchar(' ', spaces);
-	return (get_printed_len(nbr_len, spaces, zeros));
+		ft_putnchar(' ', specs.spaces);
+	return (get_printed_len(conv_spec, specs));
 }
